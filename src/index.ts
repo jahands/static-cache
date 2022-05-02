7a9472894ddb7b22d0ad83ddc5bf6cad931cbced
Jacob Hands <jacob@gogit.io>
2022-05-01 21:33:38 -0500

    Add hash + handle urls that are too long

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import sha1 from "sha1";
+
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
@@ -12,9 +14,14 @@ export default {
     if (!urlToCache) {
       return new Response("Missing url parameter", { status: 400 });
     }
+    const urlSha1 = sha1(urlToCache);
+
+    console.log("urlSha1: ", urlSha1);
     let cachedResponse: R2ObjectBody | null;
     try {
-      cachedResponse = await env.STATIC_CACHE.get(CACHE_KEY(urlToCache));
+      cachedResponse = await env.STATIC_CACHE.get(
+        CACHE_KEY(urlToCache, urlSha1)
+      );
     } catch {
       cachedResponse = null;
     }
@@ -32,7 +39,10 @@ export default {
         "Content-Disposition",
         cachedResponse.httpMetadata.contentDisposition || "inline"
       );
-      console.log('disposition ', cachedResponse.httpMetadata.contentDisposition)
+      console.log(
+        "disposition ",
+        cachedResponse.httpMetadata.contentDisposition
+      );
       if (cachedResponse.httpMetadata.contentEncoding) {
         headers.set(
           "Content-Encoding",
@@ -40,7 +50,7 @@ export default {
         );
       }
       headers.set("X-R2-Cache-Hit", "true");
-      headers.set("Content-Length", cachedResponse.size.toString())
+      headers.set("Content-Length", cachedResponse.size.toString());
       return new Response(cachedResponse.body, {
         status: 200,
         headers: headers,
@@ -51,14 +61,23 @@ export default {
       const meta: R2HTTPMetadata = {
         cacheControl: "public, max-age=604800", // 1 week
         contentType: response.headers.get("Content-Type") || "text/plain",
-        contentDisposition: response.headers.get("Content-Disposition") || undefined,
+        contentDisposition:
+          response.headers.get("Content-Disposition") || undefined,
         contentEncoding: response.headers.get("Content-Encoding") || undefined,
         contentLanguage: response.headers.get("Content-Language") || undefined,
-      }
+      };
       if (response.ok) {
-        await env.STATIC_CACHE.put(CACHE_KEY(urlToCache), response.clone().body, {
-          httpMetadata: meta
-        });
+        await env.STATIC_CACHE.put(
+          CACHE_KEY(urlToCache, urlSha1),
+          response.clone().body,
+          {
+            httpMetadata: meta,
+            customMetadata: {
+              url: urlToCache,
+              urlSha1: urlSha1,
+            },
+          }
+        );
       }
       return response;
     }
@@ -66,4 +85,8 @@ export default {
 };
 
 /** Used adds a prefix to r2 cache keys */
-const CACHE_KEY = (url: string) => `cache/${url}`;
+
+function CACHE_KEY(url: string, hash: string) {
+  // 1024 is the max length of a key in the S3 spec
+  return `cache/${url}`.slice(0, 1024 - hash.length - 7) + "--sha1=" + hash;
+}
